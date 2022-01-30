fb594a8cc97c67546ab069738daac933609fc59f
cahyoariawan21 <cahyoariawan21@gmail.com>
2022-01-30 08:57:59 +0700

    🔆String Type

---
+++ string.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Cahyo")
+
+	// Menghitung Panjang Karaker String
+	fmt.Println(len("Cahyo"))
+	// Menampilkan besaran Byte pada index 0/ karakter ke-1
+	fmt.Println("Cahyo"[0])
+	// Menampilkan besaran Byte pada index 1/ karakter ke-2
+	fmt.Println("Cahyo Ariawan"[1])
+
+}
