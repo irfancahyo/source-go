65818b3df19fc5d3efc4c1ec544599e405407c18
Irfan Cahyo <67751055+irfancahyo@users.noreply.github.com>
2022-02-12 13:23:27 +0700

    Update Params.go

--- funtion/funtion-params/params.go
+++ funtion/funtion-params/params.go
@@ -1,5 +1,15 @@
 package main
 
+import "fmt"
+
+// Function Parameter
+
+func addMahasiswa(firstNames string, lastNames string, prodi string, smt int) {
+	fmt.Println("Hello", firstNames, lastNames, "Kamu telah terdaftar di prodi", prodi, "silahkan untuk mendaftar ulang untuk memulai perkuliahan semester", smt)
+}
+
 func main() {
+
+	addMahasiswa("Irfan", "Cahyo", "Rekayasa Perangkat Lunak", 1)
 
 }
